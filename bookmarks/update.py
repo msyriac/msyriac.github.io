115850c33e4311922f0e54c0f20d5121b36b5e22
Mathew Madhavacheril <mathewsyriac@gmail.com>
2020-11-09 12:13:12 -0500

    toggle private bookmarks

--- bookmarks/update.py
+++ bookmarks/update.py
@@ -9,8 +9,10 @@ mstr = """<div class='grid-container'>$ITEM
 istr = """
   <div class='grid-item'>
 	<h3>$HEADING</h3>
+$PRIV_DIV
 	<ul>$LIST
 	</ul>
+$END_PRIV_DIV
   </div>"""
 
 lstr = """
@@ -24,14 +26,23 @@ for fname in files:
         clstr = ""
         for i,line in enumerate(f):
             if i==0:
-                heading = line.strip()
+                heading,private = line.strip().split(',')
                 continue
+            if private=='true':
+                pdiv1 = "<div class='private' style=\"display: none;\">"
+                pdiv2 = "</div>"
+            elif private=='false':
+                pdiv1 = ""
+                pdiv2 = ""
+            else:
+                raise ValueError
+                    
             flist = line.split(',')
             litem = flist[0].strip()
             link = ','.join(flist[1:]).strip()
             clstr = clstr + str(lstr).replace('$LINK',link).replace('$LITEM',litem)
     
-    cistr = cistr + str(istr).replace('$HEADING',heading).replace('$LIST',clstr)
+    cistr = cistr + str(istr).replace('$HEADING',heading).replace('$LIST',clstr).replace('$PRIV_DIV',pdiv1).replace('$END_PRIV_DIV',pdiv2)
 
         
 mstr = mstr.replace('$ITEM',cistr)
